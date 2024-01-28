3d4f8c5603d180efc46496bb9419a81b208654e3
DMBrar <dmvbrar@gmail.com>
2024-01-28 16:14:09 -0500

    update logo src

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './src/assets/logo.svg';
 
 export default function Layout() {
   return (
     <>
       <nav className='p-6 uppercase text-sm tracking-wider flex gap-6 flex-wrap flex-row justify-between items-center'>
         <Link to="/">
-          <img src="./src/assets/logo.svg" alt="Logo" className='h-5' />
+          <img src={Logo} alt="Logo" className='h-5' />
         </Link>
         <div className='flex gap-6 flex-wrap flex-row'>
           <Link to="/">Home</Link>
