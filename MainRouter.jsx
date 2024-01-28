fe5db39d4c8a1c62d74b2bd1ae60048c6c29a71f
DMBrar <dmvbrar@gmail.com>
2024-01-28 13:46:15 -0500

    update all

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './src/about'
 import Contact from './src/contact'
-import Education from './src/services'
-import Project from './src/projects'
+import Services from './src/services'
+import Projects from './src/projects'
 import Layout from './components/layout'
 
 const MainRouter = () =>
@@ -15,7 +15,7 @@ const MainRouter = () =>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/services" element={<Services />} />
-            <Route exact path="/projects" element={<Project />} />
+            <Route exact path="/projects" element={<Projects />} />
             <Route exact path="/contact" element={<Contact />} />
         </Routes>
     </div>)
